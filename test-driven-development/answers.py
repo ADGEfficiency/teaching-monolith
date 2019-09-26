fe6d746225fbe710a1d94524855cbecd82c405ba
Adam Green <adam.green@adgefficiency.com>
2019-09-26 21:19:11 +0200

    python basics - one step at a time

---
+++ test-driven-development/answers.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def test_normalize():
+    arr = np.array([[-5, 0], [0, 10], [5, -10]]).astype(np.float32)
+    norm = normalize(arr)
+
+    np.testing.assert_array_equal(norm, [[0, 0.5], [0.5, 1], [1, 0]])
+    assert norm.shape == arr.shape
+    assert (np.min(norm, axis=0) == 0).all()
+    assert (np.max(norm, axis=0) == 1.0).all()
+
+
+def normalize(arr):
+    mins = np.min(arr, axis=0)
+    maxs = np.max(arr, axis=0)
+    return (arr - mins) / (maxs - mins)
+
+
+def test_standardizer():
+    data = np.random.uniform(size=20).reshape((10, 2)) * 100
+    standardized = standardizer(data)
+    np.testing.assert_array_less(np.mean(standardized, axis=0), 1e-14)
+    np.testing.assert_allclose(np.var(standardized, axis=0), 1)
+
+
+def standardizer(x):
+    means = np.mean(x, axis=0, keepdims=True)
+    stds = np.std(x, axis=0, keepdims=True)
+    return (x - means) / stds
